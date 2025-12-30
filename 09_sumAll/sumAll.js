5c3a1ebd0ed7a27d983f95bcbf6bb1e626f7c863
markjasperrojas <markjasperrojas1@gmail.com>
2025-12-30 15:35:25 +0800

    09_sumAll: edit the code to pass the tests

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (
+    start < 0 ||
+    end < 0 ||
+    typeof start !== "number" ||
+    typeof end !== "number" ||
+    !Number.isInteger(start) ||
+    !Number.isInteger(end)
+  ) {
+    return "ERROR";
+  }
 
+  if (start > end) {
+    let temp = start;
+    start = end;
+    end = temp;
+  }
+
+  let total = 0;
+
+  for (let i = start; i <= end; i++) {
+    total += i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
