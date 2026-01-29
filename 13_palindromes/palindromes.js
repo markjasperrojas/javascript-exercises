4bc08be02b81eadc6fc9b3bff00fb8bd0103e802
markjasperrojas <markjasperrojas1@gmail.com>
2026-01-29 15:10:17 +0800

    13_palindromes: problem solved

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const newStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
 
+  return newStr.split("").reverse().join("") === newStr;
 };
 
 // Do not edit below this line
