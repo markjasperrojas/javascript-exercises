5b95cee5fe690d949212ea33c882c29ef383a2f0
markjasperrojas <markjasperrojas1@gmail.com>
2026-01-30 00:30:40 +0800

    14_fibonacci: problem solved

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) return "OOPS";
+  if (n == 1 || n == 2) return 1;
 
+  let fibonacci = 0;
+  let a = 1;
+  let b = 1;
+
+  for (let i = 0; i < Number(n) - 2; i++) {
+    fibonacci = a + b;
+    a = b;
+    b = fibonacci;
+  }
+
+  return fibonacci;
 };
 
 // Do not edit below this line
