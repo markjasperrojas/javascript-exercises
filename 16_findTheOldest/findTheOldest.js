bdba2347139ed14cc5bd3479428cb5c35beb0acb
markjasperrojas <markjasperrojas1@gmail.com>
2026-01-30 14:24:08 +0800

    16_findTheOldest: problem solved

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  const currentDate = new Date().getFullYear();
+  const newArr = array
+    .map((person) => {
+      if (person.yearOfDeath === undefined) {
+        person.yearOfDeath = currentDate;
+      }
 
+      return person;
+    })
+    .sort((firstPerson, secondPerson) => {
+      const firstPersonAge = firstPerson.yearOfDeath - firstPerson.yearOfBirth;
+      const secondPersonAge =
+        secondPerson.yearOfDeath - secondPerson.yearOfBirth;
+
+      return firstPersonAge > secondPersonAge ? -1 : 1;
+    });
+
+  return newArr[0];
 };
 
 // Do not edit below this line
